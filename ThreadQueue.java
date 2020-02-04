0f39b52dde08862e455f6e6e0407d2a5fbfd1936
shaun <spv28@cam.ac.uk>
2020-02-04 12:27:29 +0000

    basic outline of queues

---
+++ ThreadQueue.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2019 Andrew Rice <acr31@cam.ac.uk>, Alastair Beresford <arb33@cam.ac.uk>, S.P. Vickers
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package oscar;
+
+// A FIFO queue of items of type T
+public class ThreadQueue<T> {
+//TODO:  No upper limit on queue size yet
+    private static class Link<L> {
+        L val;
+        Link<L> next;
+
+        Link(L val) {
+            this.val = val;
+            this.next = null;
+        }
+    }
+
+    private Link<T> first = null;
+    private Link<T> last = null;
+
+///<summary> Put an object on the end of the queue.<\summary>
+    public synchronized void put(T val) {
+        Link<T> nextLink  = new Link(val);
+        if(first==null) {
+            first = nextLink;
+            last = nextLink;
+        }
+        else{
+            last.next = nextLink;
+            last = nextLink;
+        }
+        this.notify();
+    }
+
+    public synchronized T take() {
+        while (first == null) { // use a loop to block thread until data is available
+            try {
+                this.wait(100);
+            } catch (InterruptedException ie) {
+                // Ignored exception
+
+            }
+        }
+        //we have something to take. remove head of list and return.
+        T popVal = first.val;
+        first = first.next;
+        return popVal;
+
+
+    }
+}
