75f783d40745f7deaea58533ed6e85d93659a92e
shaun <spv28@cam.ac.uk>
2020-02-04 13:17:00 +0000

    comment outline of kernel

--- src/ThreadQueue.java
+++ src/ThreadQueue.java
@@ -13,10 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package oscar;
 
-public const int MAX_QUEUE_LENGTH = 10;
+public const int MAX_QUEUE_LENGTH = 25;
 // A FIFO queue of items of type T
 public class ThreadQueue<T> {
 
