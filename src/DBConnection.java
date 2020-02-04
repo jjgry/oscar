748ff5dbc71d24812445099b370f67d19d5a08fa
John-Joseph Gray <jjofthegray@gmail.com>
2020-02-04 16:27:49 +0000

    Added JavaDoc to DB

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -4,17 +4,19 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+/**
+ * Maintains a connection to the database and executes commands on request
+ */
 class DBConnection {
 
-  private static final String USERNAME = "jj";
-  private static final String PASSWORD = "teamoscar";
   private static final String CONNECTION = "jdbc:mysql://10.248.114.7?characterEncoding=utf8";
 
-  Connection con;
-  Statement stmt;
-  ResultSet rs;
+  private Connection con;
 
-  public DBConnection() {
+  /**
+   * Initialises a connection to the database using the provided information
+   */
+  public DBConnection(String username, String password) {
     try {
       Class.forName("com.mysql.jdbc.Driver");
     } catch (Exception e) {
@@ -22,28 +24,28 @@ class DBConnection {
     }
 
     try {
-      makeConnection();
-    } catch (SQLException e) {
-      System.err.println(e.getMessage());
-    }
-  }
-
-  private void makeConnection() throws SQLException {
-    try {
-      con = DriverManager.getConnection(CONNECTION, USERNAME, PASSWORD);
+      con = DriverManager.getConnection(CONNECTION, username, password);
     } catch (SQLException e) {
       System.err.println(e.getMessage());
     } finally {
       if (con != null) {
-        con.close();
+        try {
+          con.close();
+        } catch (SQLException e) {
+          e.printStackTrace();
+        }
       }
     }
   }
 
+  /**
+   * @param command an SQL command to be executed on the database
+   * @return a ResultSet object containing return table of the SQL query
+   * @throws SQLException when there is an error executing the SQL query
+   */
   public ResultSet execute(String command) throws SQLException {
-    stmt = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-    rs = stmt.executeQuery(command);
-    return rs;
+    Statement stmt = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+    return stmt.executeQuery(command);
   }
 
 }
--- src/DBInterface.java
+++ src/DBInterface.java
@@ -1,35 +1,79 @@
+/**
+ * Provides functions for the kernel to call to access and modify the database
+ */
 public class DBInterface {
 
   private DBConnection connection;
 
-  public DBInterface () {
-    connection = new DBConnection();
+  /**
+   * Initialises the connection to the database
+   */
+  public DBInterface (String username, String password) {
+    connection = new DBConnection("jj", "teamoscar");
   }
 
+  /**
+   * Returns a JSON representation of the reminders that are to be sent
+   */
   public void remindersToSendToday() {
 
   }
 
+  /**
+   * Returns a JSON representation of the available appointments
+   *
+   * @param doctor the doctor we want appointments for
+   * @param startDate look for dates after
+   * @param endDate look for dates before
+   */
   public void getAppointments(String doctor, String startDate, String endDate) {
 
   }
 
+  /**
+   * Update the DB to confirm the new appintment time
+   *
+   * @param appointmentID the appointment to be confirmed
+   */
   public void confirmNewTime(String appointmentID) {
 
   }
 
+  /**
+   * Update the DB to reject the new appintment time
+   *
+   * @param appointmentID the appointment to be rejected
+   */
   public void rejectNewTime(String appointmentID) {
 
   }
 
+  /**
+   * After a patient has booked an appointment with the suegery, this is how it is added
+   *
+   * @param time time of the appointment
+   * @param doctor doctor the appointment is with
+   * @param patientID patient who's appointment it is
+   */
   public void addNewAppointment(String time, String doctor, String patientID){
 
   }
 
+  /**
+   * Get the patientID associated with the given email address
+   *
+   * @param emailID the email address of the patient
+   */
   public void getPatientID(String emailID) {
 
   }
 
+  /**
+   * Not sure what this does?
+   *
+   * @param patientID the patient associated with the appointment
+   * @param appointmentID the appointment identifier
+   */
   public void confirmAppointmentExists(String patientID, String appointmentID) {
 
   }
