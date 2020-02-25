2df42f8dc78c790385ceec58d65098b3a754fc44
shaun <spv28@cam.ac.uk>
2020-02-25 12:59:44 +0000

    minor changes to remove unecessary Warnings.

--- src/main/java/oscar/Kernel.java
+++ src/main/java/oscar/Kernel.java
@@ -11,9 +11,7 @@ import java.time.LocalDateTime;
 import java.lang.Thread;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
+
 import java.util.concurrent.TimeUnit;
 import opennlp.tools.doccat.DoccatModel;
 
@@ -45,6 +43,8 @@ public class Kernel {
     SegmentQueue<OutgoingEmailMessage> OutQ;
     SegmentQueue<IncomingEmailMessage> InQ;
     boolean Sender_ON = false;
+    LinkedList<Appointment> PendingEmailOutbox;
+
 
     DoccatModel model;
 
@@ -55,14 +55,30 @@ public class Kernel {
             e.printStackTrace();
         }
     }
+    // static variable single_instance of type Singleton
+    private static Kernel single_instance = null;
 
+    // private constructor restricted to this class itself
 
-    public Kernel() throws DBInitializationException, ClassificationTypeException {
+    // static method to create instance of Singleton class
+    public static Kernel getInstance() {
+        if (single_instance == null)
+            single_instance = new Kernel();
+
+        return single_instance;
+    }
+
+    private Kernel()  {
         //SETUP
         //Initialise the queues for the Receiver -> Kernel and the Kernel -> Sender
 
         OutQ = new SegmentQueue<>();
         InQ = new SegmentQueue<>();
+        PendingEmailOutbox = new LinkedList<>();
+    }
+
+
+    public void run() throws DBInitializationException {
 
         //  Establish the Interface to the database. REMOVE credentials from hardcoding.
 
@@ -82,10 +98,6 @@ public class Kernel {
                     throw new DBInitializationException("After trying 3 times, no connection can be established.");
                 }
             }
-        }
-
-        if(DB == null){
-            System.out.println("Why is it null?");
         }
 
         //Initialise the Sender
@@ -122,7 +134,6 @@ public class Kernel {
         };
 
 
-
         Thread Major = new Thread() {
             @Override
             public void run() {
@@ -141,15 +152,13 @@ public class Kernel {
                             int appointmentID = -1;
                             try {
                                 appointmentID = Integer.parseInt(PatientResponse.getAppointmentID());
-                            }
-                            catch(Exception e){//catching parsing exception
+                            } catch (Exception e) {//catching parsing exception
                                 //no action, leave the apptID invalid at -1.
                             }
                             // 2a. Is the received email valid? check it's a patient on the database.
-                            System.out.println("    Appointment ID: "+appointmentID);
-                            System.out.println("    Sender Email: "+PatientResponse.getSenderEmailAddress());
-                            if (finalDB.confirmAppointmentExists(PatientResponse.getSenderEmailAddress(), appointmentID))
-                            {
+                            System.out.println("    Appointment ID: " + appointmentID);
+                            System.out.println("    Sender Email: " + PatientResponse.getSenderEmailAddress());
+                            if (finalDB.confirmAppointmentExists(PatientResponse.getSenderEmailAddress(), appointmentID)) {
                                 //if a valid email....
 
                                 // 2b. Fetch information on the history of this conversation, ie last response type, appointment time, doctor name, patient name.
@@ -165,8 +174,9 @@ public class Kernel {
                                     //TODO: Handle this error properly. Why is it thrown?
 
                                 }
-                                assert (p.getEmail() == PatientResponse.getSenderEmailAddress());// these really should be the same and if not our system is not designed correctly.
+                                assert (p.getEmail().equals( PatientResponse.getSenderEmailAddress()));// these really should be the same and if not our system is not designed correctly.
                                 assert (bookedAppointment.getAppID() == appointmentID);
+                                assert (C != null);
                                 switch (C.getDecision()) {
                                     case CONFIRM:
                                         //  Confirm Appt in database
@@ -190,17 +200,17 @@ public class Kernel {
                                         LinkedList<Timeslot> all_available_timeslots = new LinkedList<>();
                                         String[] availableDates = C.getDates();
                                         System.out.println("Patient-suggested times:");
-                                        System.out.println("    "+availableDates[0]+ " to "+availableDates[1]);
-                                        System.out.println("    "+availableDates[2]+ " to "+availableDates[3]);
-                                        System.out.println("    "+availableDates[4]+ " to "+availableDates[5]);
+                                        System.out.println("    " + availableDates[0] + " to " + availableDates[1]);
+                                        System.out.println("    " + availableDates[2] + " to " + availableDates[3]);
+                                        System.out.println("    " + availableDates[4] + " to " + availableDates[5]);
                                         all_available_timeslots.addAll(finalDB.getAppointments(bookedAppointment.getDoctorID(), availableDates[0], availableDates[1]));
                                         all_available_timeslots.addAll(finalDB.getAppointments(bookedAppointment.getDoctorID(), availableDates[2], availableDates[3]));
                                         all_available_timeslots.addAll(finalDB.getAppointments(bookedAppointment.getDoctorID(), availableDates[4], availableDates[5]));
                                         //TODO: Ends of the timeslot are not implemented in the database, so should be targeted if posssible
 
                                         System.out.println("DB--> Available timeslots:");
-                                        for(Timeslot T : all_available_timeslots){
-                                            System.out.println("      starting at: "+T.getStartTime());
+                                        for (Timeslot T : all_available_timeslots) {
+                                            System.out.println("      starting at: " + T.getStartTime());
                                         }
 
                                         if (all_available_timeslots.size() < 1) {
@@ -212,7 +222,6 @@ public class Kernel {
                                                     Integer.toString(appointmentID)));//We have empty strings given as time info as we have no time info!
 
                                         } else {// we have a collection of >= 1 to choose from.
-                                            //TODO: select one to suggest based on a more sensible criteria.
                                             int selectedTimeslotID = all_available_timeslots.getFirst().getID();
                                             //cancel last one.
                                             finalDB.rejectTime(appointmentID);
@@ -234,28 +243,26 @@ public class Kernel {
 
                                 }
 
-                            }
-                            else {//invalid patientID...
+                            } else {//invalid patientID...
                                 System.out.println("    <Appointment Does Not Exist>");
                                 //  Send invalidEmailAddress  Email.
                                 OutQ.put(new OutgoingEmailMessage(
                                         PatientResponse.getSenderEmailAddress(), "", "",
                                         "", "",
-                                        EmailMessageType.InvalidEmailMessage, "NO APPOINTMENT EXISTS"));
+                                        EmailMessageType.InvalidEmailMessage, "-/-"));
                                 System.out.println(
                                         "Kernel<major>: message sent to unknown user to inform them we cannot take their emails.");
                             }
                         }
                         finalDB.closeConnection();
                         System.out.println("Kernel<major>: closing connection on main thread.");
-                    }
-                    else{
+                    } else {
                         System.out.println("Kernel<major>: no new received emails");
-                        System.out.println("Kernel<major>: inQ length"+ InQ.NumWaiting());
-                        System.out.println(("Kernel<major>: outQ length "+OutQ.NumWaiting()));
+                        System.out.println("Kernel<major>: inQ length: " + InQ.NumWaiting());
+                        System.out.println(("Kernel<major>: outQ length: " + OutQ.NumWaiting()));
                     }
                     try {
-                       MINUTES.sleep(1);
+                        MINUTES.sleep(1);
                     } catch (InterruptedException e) {
                         //ignore
                     }
@@ -264,56 +271,76 @@ public class Kernel {
         };
 
 
-        if(Sender_ON) {
+        if (Sender_ON) {
 
             DBPoll.start();
             System.out.println("Kernel: Database polling system set up.");
             Major.start();
-        }
-        else{
+        } else {
             System.err.println("Without a validly set-up sender and/or receiver, this system should not attempt to" +
-                    "handle any current emails due to error likelihood.");
+                    "handle any current emails due to the likelihood of lost messages.");
         }
         // If the DBMS port is a thread in this, put it here. If it is a system with (another) producer consumer queue, check it between every handle of an incoming, and at the end of all of these.
     }
 
     public void SendNewReminders() {
-                if(DB!= null) {
-                    System.out.println("Kernel<pollDB>: beep, at " + LocalDateTime.now());
-                    DB.openConnection();
-                    System.out.println("Kernel<pollDB>: DB Connection established");
-                    List<Appointment> newAppts = DB.remindersToSendToday();
-                    System.out.println("Kernel<pollDB>: Found " + newAppts.size() + " new appointments to remind about in latest DB poll.");
-                    LinkedList<Appointment> sent_Apps = new LinkedList<>();
-                    for (Appointment A : newAppts) {
-                        //  1a. Send any initial reminder emails that are now shown as required by the database state.
-                        if (A != null) {
-                            System.out.println("Kernel<pollDB>: Appointment ID: "+A.getAppID());
-                            Patient p = DB.getPatient(A.getAppID());
-                            if(Sender_ON){
-                                OutQ.put(new OutgoingEmailMessage(p, A, EmailMessageType.InitialReminderMessage));
-                                System.out.println("Kernel<pollDB>: Sent initial reminder message about appointment " + A.getAppID());
-                                sent_Apps.add(A);
-                            }
-                            else{
-                                System.out.println("Kernel<pollDB>: no sender, so email unsent.");
-                            }
-                        }
-                        else{
-                            System.err.println("Kernel<pollDB>: appointment given by database is a NULL pointer***");
-                        }
+        if (DB != null) {
+            System.out.println("Kernel<pollDB>: beep, at " + LocalDateTime.now());
+            DB.openConnection();
+            System.out.println("Kernel<pollDB>: DB Connection established");
+            List<Appointment> newAppts = DB.remindersToSendToday();
+            newAppts.removeAll(PendingEmailOutbox);
+            System.out.println("Kernel<pollDB>: Found " + newAppts.size() + " new appointments to remind about in latest DB poll.");
+            LinkedList<Appointment> sent_Apps = new LinkedList<>();
+            for (Appointment A : newAppts) {
+                //  1a. Send any initial reminder emails that are now shown as required by the database state.
+                if (A != null) {
+                    System.out.println("Kernel<pollDB>: Appointment ID: " + A.getAppID());
+                    Patient p = DB.getPatient(A.getAppID());
+                    if (Sender_ON) {
+                        OutQ.put(new OutgoingEmailMessage(p, A, EmailMessageType.InitialReminderMessage));
+                        System.out.println("Kernel<pollDB>: Sent initial reminder message about appointment " + A.getAppID());
+                        //sent_Apps.add(A);
+                        PendingEmailOutbox.add(A);
+                    } else {
+                        System.err.println("Kernel<pollDB>: no sender, so email unsent.");
                     }
-                    DB.RemindersSent(sent_Apps);
-                    DB.closeConnection();
-                    System.out.println("Kernel<pollDB>: DB Connection ended");
-
-                }else{
-                    System.out.println("Kernel<pollDB>: Database pointer is null.");
+                } else {
+                    System.err.println("Kernel<pollDB>: appointment given by database is a NULL pointer***");
                 }
+            }
+            //DB.RemindersSent(sent_Apps);
+            DB.closeConnection();
+            System.out.println("Kernel<pollDB>: DB Connection ended");
+
+        } else {
+            System.out.println("Kernel<pollDB>: Database pointer is null.");
+        }
+    }
+
+    //TODO: Simonas needs to implement calling this.
+    public static void Confirm_Intro_Email_Sent(Appointment A) {
+        Kernel k = Kernel.getInstance();
+        k.CIES(A);
+    }
+
+    private void CIES(Appointment A) {
+
+        if (PendingEmailOutbox.remove(A)) {
+            LinkedList<Appointment> ConfirmedSent = new LinkedList<>();
+            ConfirmedSent.add(A);
+            DB.openConnection();
+            DB.RemindersSent(ConfirmedSent);
+            DB.closeConnection();
+            System.out.println("Kernel: Confirmed initial message about appointment No." + A.getAppID() + " was sent.");
+        } else {
+            System.err.println("Trying to confirm an intro email not in the pending outbox.");
+        }
     }
 
 
-    public static void main( String[] args ) throws DBInitializationException, ClassificationTypeException {
-        new Kernel();
+    public static void main(String[] args) throws DBInitializationException{
+        Kernel k = Kernel.getInstance();
+        k.run();
     }
 }
